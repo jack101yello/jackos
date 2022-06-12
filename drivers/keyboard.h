0275e481ebe54d925ace49bd4ec509ce23846326
Jack Hudson <jack.hudson12@yahoo.com>
2022-06-11 21:17:48 -0400

    Started new version from scratch

--- drivers/keyboard.h
+++
@@ -1,6 +0,0 @@
-#ifndef __keyboard_H
-#define __keyboard_H
-
-void init_keyboard();
-
-#endif
--- kernel/kernel_entry.asm
+++
@@ -1,5 +0,0 @@
-; Ensures that the kernel boots into main()
-[bits 32]
-[extern main]
-call main   ; Invoke main()
-jmp $   ; hang forever once we return from the kernel
